a7576bf6f84a59b727c00b62b6488f4ebfd7198d
manuela.ossanes <manuela.ossanes@codescript.com.br>
2025-11-21 11:52:35 -0300

    feat: connect cam

---
+++ 03-Video-Basics/01_connect_cam.py
@@ -0,0 +1,25 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+writer = cv2.VideoWriter('me.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20,(width,height))
+
+
+while True:
+    ret, frame = cap.read()
+
+    # gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    # operations (Drawing)
+
+    writer.write(frame)
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
