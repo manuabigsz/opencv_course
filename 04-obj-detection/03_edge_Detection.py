70561d61007b49cad29651b54d9c26ac65449dc5
manuela.ossanes <manuela.ossanes@codescript.com.br>
2025-11-28 15:00:29 -0300

    feat: edge detection

---
+++ 04-obj-detection/03_edge_Detection.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+img = cv2.imread('DATA/sammy_face.jpg')
+
+edges = cv2.Canny(image=img, threshold1=127, threshold2=127)
+plt.imshow(edges)
+# plt.show()
+
+edges = cv2.Canny(image=img, threshold1=0, threshold2=255)
+plt.imshow(edges)
+# plt.show()
+
+## Choosing Thresholds
+
+# Calculate the median pixel value
+med_val = np.median(img) 
+
+# Lower bound is either 0 or 70% of the median value, whicever is higher
+lower = int(max(0, 0.7* med_val))
+
+# Upper bound is either 255 or 30% above the median value, whichever is lower
+upper = int(min(255,1.3 * med_val))
+
+edges = cv2.Canny(image=img, threshold1=lower , threshold2=upper)
+
+plt.imshow(edges)
+plt.show()
+
+blurred_img = cv2.blur(img,ksize=(5,5))
+edges = cv2.Canny(image=blurred_img, threshold1=lower , threshold2=upper)
+plt.imshow(edges)
+plt.show()
+
+edges = cv2.Canny(image=blurred_img, threshold1=lower , threshold2=upper+50)
+plt.imshow(edges)
+plt.show()
